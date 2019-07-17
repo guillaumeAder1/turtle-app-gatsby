a8864cc93521556fb6c737980ca2252c64d44141
guillaume <guillaume@takewalks.com>
2019-07-17 14:37:57 +0100

    start adding more controls

--- src/components/game/controls.js
+++ src/components/game/controls.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 const Control = props => {
+  const [count, setCount] = useState(3)
+
   const move = () => {
     props.callback('move')
   }
   const rotate = () => {
     props.callback('rotate')
   }
+  const showHide = () => {
+    setCount(count - 1)
+    if (count > 0) {
+      const allMask = document.querySelectorAll('.cell .mask')
+      allMask.forEach( e => e.classList.add('display') )
+      setTimeout(() => {
+        allMask.forEach( e => e.classList.remove('display') )
+      }, 1000)
+    }
+  }
+  
   return (
     <div>
       <button onClick={() => move()}>Move</button>
-      <button onClick={() => rotate()}>Rotate</button>
+      <button onClick={() => rotate()}>Rotate</button>      
+      <button onClick={() => showHide() }>show/hide {count}</button>
     </div>
   )
 }
