ecfd6cd16b5a912d014458eb81a72d11f6962bb8
guillaume <guillaume@takewalks.com>
2019-07-19 16:43:55 +0100

    generate grid dynamically

--- src/pages/turtle-game.js
+++ src/pages/turtle-game.js
@@ -2,9 +2,22 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Grid from '../components/turtleGame/grid';
+import { randomRange } from '../components/turtleGame/utils'
+const min = 5
+const max = 10
+const gridWidth = randomRange(min, max)
+const gridHeight = randomRange(min, max)
+const minesMin = (min * min) / 2
+const minesMax = (max * max) * 0.75
+const nbrMine = randomRange(2, minesMax/2)
+const mines = new Array(nbrMine).fill(null).map( e => {
+  return  { 
+    x: randomRange(2, max), 
+    y:randomRange(2, max) } 
+})
+console.log(mines)
 
-const sizeGrid = { w: 10, h: 5 }
-const mines = [{ x: 2, y: 3 }, { x: 3, y: 3 }];
+const sizeGrid = { w: gridWidth, h: gridHeight }
 const IndexPage = () => (
   <Layout>
     <SEO title="Game" keywords={[`gatsby`, `game`, `react`]} />
