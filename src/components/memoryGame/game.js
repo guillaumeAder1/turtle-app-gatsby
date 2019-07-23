32a3ef5666050d942d0ce85620095cada12cd567
Guillaume Ader <guillaume@Guillaumes-MacBook-Pro-2.local>
2019-07-23 21:16:11 +0100

    start refactoring hide display selected

--- src/components/memoryGame/game.js
+++ src/components/memoryGame/game.js
@@ -3,7 +3,7 @@ import { symbol } from 'prop-types';
 
 class Game extends Component {
   constructor(props) {
-    super(props);   
+    super(props);
     this.numbers = {
       1: 'X',
       2: 'Y',
@@ -26,55 +26,91 @@ class Game extends Component {
   }
   select(e) {
     const key = this.getValue(e);
-    const len = Object.keys(this.state.temp).length
+    const len = Object.keys(this.state.temp).length;
     // first click
-    if (!this.state.temp[key] && len === 0) {
+    if (!this.state.temp[key]) {
       this.state.temp[key] = [e];
-      this.setState({
-        temp: { ...this.state.temp },
-        counter: this.state.counter + 1,
-      });
-    } else {
-      let found
-      if (this.state.temp[key] && this.state.temp[key].indexOf(e) === -1 ) {
-        found = key
-        for(let i in this.numbers) {
-          if(this.numbers[i] === key) {
-            delete this.numbers[i]
+      this.setState(
+        {
+          temp: { ...this.state.temp },
+          counter: this.state.counter + 1,
+        },
+        () => {
+          if (this.state.counter === 2) {
+            setTimeout(() => {
+              this.setState({
+                temp: {},
+                counter: 0,
+              });
+            }, 1000);
+          }
+        },
+      );
+    } else if (this.state.temp[key][0] !== e) {
+      this.state.temp[key].push(e);
+      this.setState(
+        {
+          temp: { ...this.state.temp },
+          counter: this.state.counter + 1,
+        },
+        () => {
+          setTimeout(() => {
+            let match = [];
+            if (Object.keys(this.state.temp[key]).length === 2) {
+              match = [key];
+              for (let i in this.numbers) {
+                if (this.numbers[i] === key) {
+                  delete this.numbers[i];
+                }
+              }
+            }
+            this.setState(
+              {
+                temp: {},
+                counter: 0,
+                found: this.state.found.concat(match),
+              },
+              () => {
+                console.log(this.numbers, this.state.found);
+              },
+            );
+          }, 1000);
+        },
+      );
+      return;
+      let found;
+      if (this.state.temp[key] && this.state.temp[key].indexOf(e) === -1) {
+        found = key;
+        for (let i in this.numbers) {
+          if (this.numbers[i] === key) {
+            delete this.numbers[i];
           }
         }
-        console.log(this.numbers)
+        console.log(this.numbers);
       } else {
-        found = []
+        found = [];
       }
-
-      this.setState({
-        temp: {},
-        counter: 0,
-        found: this.state.found.concat(found)
-      }, () => {
-        console.log(this.state.found, this.state.temp)
-      })
-      
     }
   }
   getValue(key) {
     return this.numbers[key];
   }
   isDisplay(val) {
-    const key = this.getValue(val)
-    const isSelected = !!(this.state.temp[key] && this.state.temp[key].indexOf(val) > -1)
-    return this.state.visible || isSelected ? this.numbers[val] : ''
+    const key = this.getValue(val);
+    const isSelected = !!(
+      this.state.temp[key] && this.state.temp[key].indexOf(val) > -1
+    );
+    return this.state.visible || isSelected ? this.numbers[val] : '';
   }
-  isVisible(){
-    return ''
+  isVisible() {
+    return '';
   }
   componentDidMount() {
     setTimeout(() => {
       this.setState({
-        visible: false
-      })
-    }, 2000)
+        visible: false,
+      });
+    }, 2000);
   }
 
   render() {
@@ -83,9 +119,15 @@ class Game extends Component {
         {this.state.counter > 0 && (
           <div>looking for: {Object.keys(this.state.temp)[0]}</div>
         )}
-        {this.state.found.length > 0 && ( <div>Symbol found: {this.state.found.join(', ')}</div> )}
+        {this.state.found.length > 0 && (
+          <div>Symbol found: {this.state.found.join(', ')}</div>
+        )}
         {this.state.gridLength.map((e, i) => (
-          <div key={i} onClick={() => this.select(e)} className={"cell " + this.isVisible(e)}>
+          <div
+            key={i}
+            onClick={() => this.select(e)}
+            className={'cell ' + this.isVisible(e)}
+          >
             {this.isDisplay(e)}
           </div>
         ))}
