667ec780aa55a2dec4bd0c2602e8f49c0fe85582
guillaume <guillaume@takewalks.com>
2019-07-25 15:04:39 +0100

    Updates

---
+++ component---src-pages-page-2-js-849614bc66b3bf132305.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{158:function(e,t,o){"use strict";o.r(t);var n=o(8),b=(o(0),o(166)),c=o(168),a=o(167);t.default=function(){return Object(n.b)(c.a,null,Object(n.b)(a.a,{title:"Page two"}),Object(n.b)("h1",null,"Hi from the second page"),Object(n.b)("p",null,"Welcome to page 2"),Object(n.b)(b.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-849614bc66b3bf132305.js.map
---
+++ styles-2d8df0a9d849cb73b0e7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{182:function(n,o,w){},183:function(n,o,w){}}]);
+//# sourceMappingURL=styles-2d8df0a9d849cb73b0e7.js.map
